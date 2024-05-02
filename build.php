c14ee3e47a0566ca61105930845e12eab96c722f
jpâmela_root <59771746+jpamela-root@users.noreply.github.com>
2024-05-02 13:06:16 -0300

    Update build.php

--- build.php
+++ build.php
@@ -1,6 +1,16 @@
 <?php
-if (!empty($_POST['link'])&&!empty($_POST['short'])) {
-  $link=$_POST["link"];
-  
+if (!empty($_POST['link']) && !empty($_POST['short'])) {
+    $link = $_POST["link"];
+    $short = $_POST["short"];
+
+    // Adicione aqui a lógica para encurtar o link
+
+    echo "Link original: $link<br>";
+    echo "Link encurtado: $short";
+} else {
+    echo "Por favor, insira um link original e um link encurtado.";
+}
+?>
+
 
 /*um dia eu termino */
